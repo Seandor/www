ca2fcaedb069719084f2e18a008f4685f831bcc2
Seandor <seandor123@gmail.com>
2016-03-21 10:11:17 +0800

    optimization on the digits and balls release.

--- canvas/countdown.js
+++ canvas/countdown.js
@@ -1,5 +1,5 @@
 var WINDOW_WIDTH = 1024;
-var WINDOW_HEIGHT = 768;
+var WINDOW_HEIGHT = 600;
 var RADIUS = 8;
 var MARGIN_TOP = 30;
 var MARGIN_LEFT = 60;
@@ -33,30 +33,32 @@ window.onload = function() {
 function update() {
 	next_time = new Date();
 	if (cur_time.getSeconds() != next_time.getSeconds()) {
-		if ((cur_time.getHours()/10) != (next_time.getHours()/10)) {
+		if (parseInt(cur_time.getHours()/10) != parseInt(next_time.getHours()/10)) {
 			addBalls(MARGIN_LEFT, MARGIN_TOP, next_time.getHours()/10);
 		}		
-		if ((cur_time.getHours()%10) != (next_time.getHours()%10)) {
+		if (parseInt(cur_time.getHours()%10) != parseInt(next_time.getHours()%10)) {
 			addBalls(MARGIN_LEFT + NUM_WIDTH+PADDING, MARGIN_TOP, next_time.getHours()%10);
 		}
 
-		if ((cur_time.getMinutes()/10) != (next_time.getMinutes()/10)) {
+		if (parseInt(cur_time.getMinutes()/10) != parseInt(next_time.getMinutes()/10)) {
 			addBalls(MARGIN_LEFT + 2*(NUM_WIDTH+PADDING) + COLON_WIDTH+PADDING, MARGIN_TOP, next_time.getMinutes()/10);
 		}		
-		if ((cur_time.getMinutes()%10) != (next_time.getMinutes()%10)) {
+		if (parseInt(cur_time.getMinutes()%10) != parseInt(next_time.getMinutes()%10)) {
 			addBalls(MARGIN_LEFT + 3*(NUM_WIDTH+PADDING) + COLON_WIDTH+PADDING, MARGIN_TOP, next_time.getMinutes()%10);
 		}
 
-		if ((cur_time.getSeconds()/10) != (next_time.getSeconds()/10)) {
+		if (parseInt(cur_time.getSeconds()/10) != parseInt(next_time.getSeconds()/10)) {
 			addBalls(MARGIN_LEFT + 4*(NUM_WIDTH+PADDING) + 2*(COLON_WIDTH+PADDING), MARGIN_TOP, parseInt(next_time.getSeconds()/10));
 		}		
-		if ((cur_time.getSeconds()%10) != (next_time.getSeconds()%10)) {
+		if (parseInt(cur_time.getSeconds()%10) != parseInt(next_time.getSeconds()%10)) {
 			addBalls(MARGIN_LEFT + 5*(NUM_WIDTH+PADDING) + 2*(COLON_WIDTH+PADDING), MARGIN_TOP, parseInt(next_time.getSeconds()%10));
 		}
 		cur_time = next_time;
 	}
 
 	updateBalls();
+
+	// console.log(balls.length);
 }
 
 function addBalls(x, y, num) {
@@ -67,7 +69,7 @@ function addBalls(x, y, num) {
 					x: x+(RADIUS+1)+j*(2*RADIUS+1),
 					y: y+(RADIUS+1)+i*(2*RADIUS+1),
 					g: 1.5+Math.random(),
-					vx: Math.pow(-1, Math.ceil(Math.random()*1000)) * 4,
+					vx: Math.pow(-1, Math.ceil(Math.random()*1000)) * 5,
 					vy: -5,
 					color: colors[Math.floor(Math.random()*colors.length)]
 				};
@@ -85,7 +87,24 @@ function updateBalls() {
 		if (balls[i].y > (WINDOW_HEIGHT - RADIUS)) {
 			balls[i].y = WINDOW_HEIGHT - RADIUS;
 			balls[i].vy = -balls[i].vy * 0.6;
-		} 
+		}
+		if (balls[i].x < 0) {
+			// balls.splice(i,1); 
+			balls[i].vx = -balls[i].vx
+		}
+		if (balls[i].x > (WINDOW_WIDTH - RADIUS)) {
+			balls[i].x = WINDOW_WIDTH - RADIUS;
+			balls[i].vx = -balls[i].vx;
+		}
+	}
+
+	var count = 0;
+	for (var i = 0; i < balls.length; i++) {
+		if (balls[i].x - RADIUS > 0 && balls[i].x + RADIUS < WINDOW_WIDTH)
+			balls[count++] = balls[i]
+	}
+	while (balls.length > count) {
+		balls.pop();
 	}
 }
 
